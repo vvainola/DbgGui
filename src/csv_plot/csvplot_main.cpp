79d4869d4067e90c98eeb3fef1fe18ac1a8db7a6
Vili Väinölä <vilivainola@gmail.com>
2022-12-06 14:22:49 +0200

    Start without arguments

--- src/csv_plot/csvplot_main.cpp
+++ src/csv_plot/csvplot_main.cpp
@@ -4,10 +4,6 @@
 
 int main(int argc, char** argv) {
     std::string csv_file;
-    if (argc == 1) {
-        std::cerr << "No csv file given";
-        return -1;
-    }
     std::vector<std::string> files;
     for (int i = 1; i < argc; ++i) {
         files.push_back(argv[i]);
