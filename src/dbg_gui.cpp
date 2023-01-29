a6116eb7dfb7840eb35e48226870302851ae7177
Vili Väinölä <vilivainola@gmail.com>
2023-01-29 16:31:18 +0200

    Always use PDB file instead of json in gui

--- src/dbg_gui.cpp
+++ src/dbg_gui.cpp
@@ -31,8 +31,7 @@ static void glfw_error_callback(int error, const char* description) {
 }
 
 DbgGui::DbgGui(double sampling_time)
-    : m_sampling_time(sampling_time),
-      m_dbghelp_symbols("symbols.json", false) {
+    : m_sampling_time(sampling_time) {
     assert(sampling_time > 0);
 }
 
