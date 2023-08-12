15b9d29d826d111df6ff8456ccec775f7952dd23
Vili Väinölä <vilivainola@gmail.com>
2023-08-12 13:54:59 +0300

    Remove redundant check for ImGuiConfigFlags_ViewportsEnable
    ImGuiConfigFlags_ViewportsEnable is always true. Also
    call to glfwGetCurrentContext is not needed

--- src/dbg_gui.cpp
+++ src/dbg_gui.cpp
@@ -182,7 +182,6 @@ void DbgGui::updateLoop() {
     ImGui::CreateContext();
     ImPlot::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
-    (void)io;
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport
@@ -295,16 +294,9 @@ void DbgGui::updateLoop() {
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         // Update and Render additional Platform Windows
-        // (Platform functions may change the current OpenGL context, so we
-        // save/restore it to make it easier to paste this code elsewhere.
-        //  For this specific demo app we could also call
-        //  glfwMakeContextCurrent(window) directly)
-        if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-            GLFWwindow* backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
-        }
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(m_window);
         glfwSwapBuffers(m_window);
     }
 
