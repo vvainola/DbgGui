vvainola/DbgGui

commit eb936a75464e5846d5d29045da887babfd4c2007

fix snapshot saving to memory within dlls
The module info was the main module so pointers within dlls were
not restored to the original symbol. Snapshot saving to file still
does not work for DLLs as those should use the pointer address offset.

src/symbols/dbghelp_symbols_lookup.cpp
@@ -345,7 +345,6 @@ void DbgHelpSymbols::saveSnapshotToFile(std::string const& json) const {
 
 std::vector<SymbolValue> DbgHelpSymbols::saveSnapshotToMemory() const {
     std::vector<SymbolValue> snapshot;
-    auto module_info = getCurrentModuleInfo();
     std::function<void(VariantSymbol*)> save_symbol_to_snapshot = [&](VariantSymbol* sym) {
         // Add symbol value to snapshot
         VariantSymbol::Type type = sym->getType();
@@ -353,12 +352,11 @@ std::vector<SymbolValue> DbgHelpSymbols::saveSnapshotToMemory() const {
             snapshot.push_back({sym, sym->read()});
         } else if (type == VariantSymbol::Type::Pointer) {
             MemoryAddress pointed_address = sym->getPointedAddress();
-            MemoryAddress pointed_address_offset = sym->getPointedAddress() - module_info.base_address;
-            // Set pointer only if it points to something else within this module
-            bool pointed_address_ok = pointed_address_offset < module_info.size;
+            VariantSymbol* pointed_symbol = sym->getPointedSymbol();
+            // Set pointer only if it points to some other global
             if (pointed_address == NULL) {
-                snapshot.push_back({sym, MemoryAddress(0)});
-            } else if (pointed_address_ok) {
+                snapshot.push_back({sym, MemoryAddress(NULL)});
+            } else if (pointed_symbol) {
                 snapshot.push_back({sym, sym->getPointedAddress()});
             }
         }

commit bb8fd7c8d214313976c73b5a91bbac3e44a1e809

Fix function pointers not restored from memory snapshot
This got accidentally broken in eb936a75464e5846. The snapshot restore
from file is still broken for function pointers within DLLs

src/symbols/dbghelp_symbols_lookup.cpp
@@ -246,25 +246,18 @@ bool DbgHelpSymbols::loadSymbolsFromJson(std::string const& json) {
 }
 
 void DbgHelpSymbols::loadSymbolsFromPdb() {
-    // Symbols are not loaded until a reference is made requiring the symbols be loaded.
-    // This is the fastest, most efficient way to use the symbol handler.
-    SymSetOptions(SYMOPT_DEFERRED_LOADS);
-
-    HANDLE current_process = GetCurrentProcess();
-    if (!SymInitialize(current_process, NULL, TRUE)) {
-        std::cerr << "SymInitialize failed with error:\n";
-        printLastError();
-        std::cerr << "Unable to load symbols from PDB file.\n";
+    ScopedSymbolHandler symbol_handler;
+    if (!symbol_handler.initialized()) {
         return;
     }
 
     // Collect symbol infos into vector
     std::vector<SymbolInfo> symbols;
-    if (SymEnumSymbols(current_process, // Process handle from SymInitialize.
-                       0,               // Base address of module.
-                       "*!*",           // Name of symbols to match.
-                       storeSymbols,    // Symbol handler procedure.
-                       &symbols))       // User context.
+    if (SymEnumSymbols(symbol_handler.getCurrentProcess(), // Process handle from SymInitialize.
+                       0,                                  // Base address of module.
+                       "*!*",                              // Name of symbols to match.
+                       storeSymbols,                       // Symbol handler procedure.
+                       &symbols))                          // User context.
     {
         // SymEnumSymbols succeeded
     } else {
@@ -300,7 +293,6 @@ void DbgHelpSymbols::loadSymbolsFromPdb() {
         addChildrenToSymbol(*raw_symbol, reference_symbols);
         m_root_symbols.push_back(std::make_unique<VariantSymbol>(m_root_symbols, raw_symbol.get()));
     }
-    SymCleanup(current_process);
 }
 
 void DbgHelpSymbols::saveSnapshotToFile(std::string const& json) const {
@@ -344,6 +336,8 @@ void DbgHelpSymbols::saveSnapshotToFile(std::string const& json) const {
 }
 
 std::vector<SymbolValue> DbgHelpSymbols::saveSnapshotToMemory() const {
+    ScopedSymbolHandler scoped_symbol_handler;
+
     std::vector<SymbolValue> snapshot;
     std::function<void(VariantSymbol*)> save_symbol_to_snapshot = [&](VariantSymbol* sym) {
         // Add symbol value to snapshot
@@ -358,6 +352,12 @@ std::vector<SymbolValue> DbgHelpSymbols::saveSnapshotToMemory() const {
                 snapshot.push_back({sym, MemoryAddress(NULL)});
             } else if (pointed_symbol) {
                 snapshot.push_back({sym, sym->getPointedAddress()});
+            } else {
+                // Try finding the symbol with directly from address with symbol handler
+                auto raw_sym = getSymbolFromAddress(pointed_address);
+                if (raw_sym) {
+                    snapshot.push_back({sym, pointed_address});
+                }
             }
         }
         // Add all children
@@ -368,6 +368,7 @@ std::vector<SymbolValue> DbgHelpSymbols::saveSnapshotToMemory() const {
     for (std::unique_ptr<VariantSymbol> const& sym : m_root_symbols) {
         save_symbol_to_snapshot(sym.get());
     }
+
     return snapshot;
 }
 

src/symbols/symbol_helpers.h
@@ -25,6 +25,7 @@
 #include <optional>
 #include <memory>
 #include <map>
+#include <iostream>
 
 using ModuleBase = ULONG64;
 using TypeIndex = ULONG;
@@ -69,3 +70,36 @@ std::string readFile(std::string const& filename);
 
 // Currently unused helpers
 DataKind getDataKind(RawSymbol const& sym);
+
+class ScopedSymbolHandler {
+  public:
+    ScopedSymbolHandler() {
+        // Symbols are not loaded until a reference is made requiring the symbols be loaded.
+        // This is the fastest, most efficient way to use the symbol handler.
+        SymSetOptions(SYMOPT_DEFERRED_LOADS);
+        m_symbol_handler_initialized = SymInitialize(m_current_process, NULL, TRUE);
+        if (!m_symbol_handler_initialized) {
+            std::cerr << "SymInitialize failed with error:\n";
+            printLastError();
+            std::cerr << "Unable to load symbols from PDB file.\n";
+        }
+    }
+
+    bool initialized() {
+        return m_symbol_handler_initialized;
+    }
+
+    HANDLE getCurrentProcess() {
+        return m_current_process;
+    }
+
+    ~ScopedSymbolHandler() {
+        if (m_symbol_handler_initialized) {
+            SymCleanup(m_current_process);
+        }
+    }
+
+  private:
+    bool m_symbol_handler_initialized = false;
+    HANDLE m_current_process = GetCurrentProcess();
+};

src/symbols/variant_symbol.cpp
@@ -219,11 +219,7 @@ std::string VariantSymbol::valueAsStr() const {
                 return it->second;
             }
 
-            SymSetOptions(SYMOPT_DEFERRED_LOADS);
-            bool symbol_handler_initialized = SymInitialize(GetCurrentProcess(), NULL, TRUE);
-            if (!symbol_handler_initialized) {
-                printLastError();
-            }
+            ScopedSymbolHandler scoped_symbol_handler;
             auto sym = getSymbolFromAddress(pointed_address);
             std::string name = "??";
             if (sym) {
@@ -235,9 +231,6 @@ std::string VariantSymbol::valueAsStr() const {
                     name = getUndecoratedSymbolName(decorated_name);
                 }
                 symbol_addresses[pointed_address] = name;
-            }
-            if (symbol_handler_initialized) {
-                SymCleanup(GetCurrentProcess());
             }
             return name;
         }