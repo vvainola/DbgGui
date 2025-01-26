b4886130bf5c09febd40d5a559799c172e1a9537
Vili Väinölä <vilivainola@gmail.com>
2025-01-26 11:01:46 +0200

    Remove duplicate evaluateExpression

--- src/csv_plot/csvplot_custom_signal.cpp
+++ src/csv_plot/csvplot_custom_signal.cpp
@@ -21,6 +21,7 @@
 // SOFTWARE.
 
 #include "csvplot.h"
+#include "str_helpers.h"
 
 #include <stack>
 #include <stdexcept>
@@ -29,141 +30,6 @@
 inline int MAX_CUSTOM_SIGNALS_IN_EQ = 10;
 inline int MAX_CUSTOM_EQ_LENGTH = 1000;
 inline int MAX_CUSTOM_EQ_NAME = 256;
-
-static bool isOperator(char c);
-static int getPrecedence(char op);
-static double evaluateExpression(std::istringstream& iss);
-static double evaluateExpression(std::string expression);
-static std::string getFormattedEqForSample(std::string_view fmt, std::vector<CsvSignal*> const& signals, int i);
-
-static bool isOperator(char c) {
-    return c == '+' || c == '-' || c == '*' || c == '/';
-}
-
-static int getPrecedence(char op) {
-    if (op == '+' || op == '-') {
-        return 1;
-    } else if (op == '*' || op == '/') {
-        return 2;
-    }
-    return 0; // Default precedence for non-operators
-}
-
-static double applyOperator(double operand1, double operand2, char op) {
-    switch (op) {
-        case '+': return operand1 + operand2;
-        case '-': return operand1 - operand2;
-        case '*': return operand1 * operand2;
-        case '/': return operand1 / operand2;
-        default:
-            throw std::runtime_error(std::format("Invalid operator: {}", op));
-    }
-}
-
-static double evaluateExpression(std::istringstream& iss) {
-    std::stack<double> operand_stack;
-    std::stack<char> operator_stack;
-    std::stack<char> full_stack;
-
-    auto evaluateOperatorStack = [&]() {
-        char top_operator = operator_stack.top();
-        operator_stack.pop();
-
-        if (operand_stack.size() < 2) {
-            throw std::runtime_error("Invalid expression: too few operands");
-        }
-        double operand2 = operand_stack.top();
-        operand_stack.pop();
-        double operand1 = operand_stack.top();
-        operand_stack.pop();
-
-        double result = applyOperator(operand1, operand2, top_operator);
-        operand_stack.push(result);
-    };
-
-    char current_char;
-    while (iss.get(current_char)) {
-        // Digit
-        // Unary operator
-        // Unary operator preceded by operator e.g. "1 + -2" or "1 / -(2)"
-        if (isdigit(current_char)
-            || (current_char == '-' && operand_stack.empty() && (isdigit(iss.peek()) || iss.peek() == '('))
-            || (current_char == '-' && !full_stack.empty() && isOperator(full_stack.top()) && (isdigit(iss.peek()) || iss.peek() == '('))) {
-            // Parse a number
-            double operand;
-            if (iss.peek() == '(') {
-                iss.get(); // Remove opening parenthesis
-                operand = -evaluateExpression(iss);
-            } else {
-                iss.putback(current_char);
-                iss >> operand;
-            }
-            operand_stack.push(operand);
-            // Don't care about the value in full stack but it has to be distinguishable from operator
-            full_stack.push('0');
-        }
-        // sqrt
-        else if (current_char == 's') {
-            assert(iss.get() == 'q');
-            assert(iss.get() == 'r');
-            assert(iss.get() == 't');
-            assert(iss.get() == '(');
-            double operand = evaluateExpression(iss);
-            operand_stack.push(sqrt(operand));
-            full_stack.push('0');
-        } else if (isOperator(current_char)) {
-            // Token is an operator
-            char current_operator = current_char;
-
-            while (!operator_stack.empty() && getPrecedence(operator_stack.top()) >= getPrecedence(current_operator)) {
-                // Apply higher or equal precedence operators on top of the operator stack
-                evaluateOperatorStack();
-            }
-
-            // Push the current operator onto the stack
-            operator_stack.push(current_operator);
-            full_stack.push(current_operator);
-        } else if (current_char == '(') {
-            // Token is an opening parenthesis, evaluate the expression inside the parenthesis
-            double result = evaluateExpression(iss);
-            operand_stack.push(result);
-            // Don't care about the value in full stack but it has to be distinguishable from operator
-            full_stack.push('0');
-        } else if (current_char == ')') {
-            // Token is a closing parenthesis, evaluate the expression
-            while (!operator_stack.empty()) {
-                evaluateOperatorStack();
-            }
-            if (operand_stack.size() != 1) {
-                throw std::runtime_error(std::format("Invalid number of operands"));
-            }
-            return operand_stack.top();
-        } else {
-            throw std::runtime_error(std::format("Invalid character in expression: {}", current_char));
-            return 0.0; // Handle invalid characters gracefully
-        }
-    }
-
-    // Process the remaining operators in the stack
-    while (!operator_stack.empty()) {
-        evaluateOperatorStack();
-    }
-
-    // The final result is on top of the operand stack
-    if (operand_stack.size() == 1) {
-        return operand_stack.top();
-    } else {
-        throw std::runtime_error(std::format("Invalid expression: too many operands"));
-        return 0.0;
-    }
-}
-
-static double evaluateExpression(std::string expression) {
-    // Remove whitespace
-    expression.erase(std::remove_if(expression.begin(), expression.end(), isspace), expression.end());
-    std::istringstream iss(expression);
-    return evaluateExpression(iss);
-}
 
 static std::string getFormattedEqForSample(std::string_view fmt, std::vector<CsvSignal*> const& signals, int i) {
     switch (signals.size()) {
@@ -296,10 +162,14 @@ void CsvPlotter::showCustomSignalCreator() {
             c.file = m_selected_signals[0]->file;
             for (int i = 0; i < m_selected_signals[0]->samples.size(); ++i) {
                 std::string expr = getFormattedEqForSample(custom_signal_eq, m_selected_signals, i);
-                c.samples.push_back(evaluateExpression(expr));
+                auto expr_value = str::evaluateExpression(expr);
+                if (!expr_value.has_value()) {
+                    m_error_message = expr_value.error();
+                    return;
+                }
+                c.samples.push_back(expr_value.value());
             }
-            m_selected_signals[0]->file->signals.push_back(c);
-
+            m_selected_signals[0]->file->signals.push_back(std::move(c));
         } catch (std::runtime_error e) {
             m_error_message = e.what();
             return;
