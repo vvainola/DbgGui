73c2719be87caf542d73488354fc4bda73ab1600
Vili Väinölä <vilivainola@gmail.com>
2025-04-04 20:14:18 +0300

    Make gray a bit more gray
    Hard to see difference between white and gray in some themes

--- src/dbg_gui.h
+++ src/dbg_gui.h
@@ -39,7 +39,7 @@
 struct GLFWwindow;
 
 inline constexpr int32_t ALL_SAMPLES = 1000'000'000;
-inline constexpr ImVec4 COLOR_GRAY = ImVec4(0.7f, 0.7f, 0.7f, 1);
+inline constexpr ImVec4 COLOR_GRAY = ImVec4(0.6f, 0.6f, 0.6f, 1);
 inline constexpr ImVec4 COLOR_TEAL = ImVec4(0.0f, 1.0f, 1.0f, 1);
 inline constexpr ImVec4 COLOR_WHITE = ImVec4(1, 1, 1, 1);
 inline constexpr int32_t MIN_FONT_SIZE = 1;
